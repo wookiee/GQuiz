0d633a5a0331abaddc200e39e8205e2f9bb3eb40
Mikey Ward <mw@mikey.io>
2023-11-09 09:50:44 +0800

    Add AutoLayout constraints to LoginVC
    Also disable autoresizing mask for the constrained views

--- Quiz/User Interface/LoginViewController.swift
+++ Quiz/User Interface/LoginViewController.swift
@@ -20,12 +20,14 @@ class LoginViewController: UIViewController {
         addLoginLabel()
         addLoginField()
         addLoginButton()
+        addLayoutConstraints()
     }
     
     private func addLoginLabel() {
         loginLabel = UILabel()
         loginLabel.text = "Username:"
         view.addSubview(loginLabel)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func addLoginButton() {
@@ -35,12 +37,34 @@ class LoginViewController: UIViewController {
         loginButton = UIButton(type: .system,
                                primaryAction: onTapAction)
         view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func addLoginField() {
         loginField = UITextField()
         view.addSubview(loginField)
+        loginField.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    private func addLayoutConstraints() {
+        
+        loginField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        loginLabel.setContentHuggingPriority(.required, for: .horizontal)
+        
+        loginField.leadingAnchor.constraint(equalTo: loginLabel.trailingAnchor, constant: 20).isActive = true
+        
+        view.layoutMarginsGuide.leadingAnchor.constraint(equalTo: loginLabel.leadingAnchor, constant: -20).isActive = true
+        
+        loginLabel.firstBaselineAnchor.constraint(equalTo: loginField.firstBaselineAnchor).isActive = true
+        
+        loginField.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1.0).isActive = true
+        
+        loginField.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -20).isActive = true
+        
+        loginField.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -20).isActive = true
+        
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+    }
     
 }
