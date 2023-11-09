9f1238423eb689f817f54ae9705efcb587eacc22
Mikey Ward <mw@mikey.io>
2023-11-09 09:46:17 +0800

    Move files into groups for better organization

--- Quiz/Model/QuizItemStore.swift
+++ Quiz/Model/QuizItemStore.swift
@@ -8,7 +8,7 @@ class QuizItemStore {
                  answer: "14"),
         QuizItem(question: "What is cognac made from?", 
                  answer: "Grapes"),
-        QuizItem(question: "What is the speed of light?", 
+        QuizItem(question: "What is the speed of light? Are you sure? Are you really sure?", 
                  answer: "186k miles/sec")
     ]
     
