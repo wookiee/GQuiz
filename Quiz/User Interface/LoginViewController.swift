4eb8e38e64b1c5148894282ba2d226fd9854ae57
Mikey Ward <mw@mikey.io>
2023-11-09 09:49:55 +0800

    Add loginLabel, loginField, loginButton to LoginVC

--- Quiz/User Interface/LoginViewController.swift
+++ Quiz/User Interface/LoginViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    var loginLabel: UILabel!
+    var loginField: UITextField!
+    var loginButton: UIButton!
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -14,6 +17,29 @@ class LoginViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
+        addLoginLabel()
+        addLoginField()
+        addLoginButton()
+    }
+    
+    private func addLoginLabel() {
+        loginLabel = UILabel()
+        loginLabel.text = "Username:"
+        view.addSubview(loginLabel)
+    }
+    
+    private func addLoginButton() {
+        let onTapAction = UIAction(title: "Login") { _ in
+            print("We're logging in")
+        }
+        loginButton = UIButton(type: .system,
+                               primaryAction: onTapAction)
+        view.addSubview(loginButton)
+    }
+    
+    private func addLoginField() {
+        loginField = UITextField()
+        view.addSubview(loginField)
     }
     
     
