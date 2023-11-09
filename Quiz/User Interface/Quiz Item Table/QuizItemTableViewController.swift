a29f4733355d86e5d98ded568d8fd1328ad399dd
Mikey Ward <mw@mikey.io>
2023-11-09 16:21:15 +0800

    Add search/filter to QuizItemTableViewController

--- Quiz/User Interface/Quiz Item Table/QuizItemTableViewController.swift
+++ Quiz/User Interface/Quiz Item Table/QuizItemTableViewController.swift
@@ -5,19 +5,54 @@ class QuizItemTableViewController: UITableViewController {
     
     let store = QuizItemStore.shared
     
+    lazy var shownItems = store.items {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        store.items.count
+        shownItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuizItemCell") as! QuizItemCell
         
-        let item = store.items[indexPath.row]
+        let item = shownItems[indexPath.row]
         cell.questionLabel.text = item.question
         cell.answerLabel.text = item.answer
         
         return cell
     }
     
+}
+
+extension QuizItemTableViewController: UISearchControllerDelegate, UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else {
+            shownItems = store.items
+            return
+        }
+        
+        let filteredItems = store.items.filter { item in
+            item.question.localizedCaseInsensitiveContains(searchText) ||
+            item.answer.localizedCaseInsensitiveContains(searchText)
+        }
+        shownItems = filteredItems
+    }
+    
+    func didDismissSearchController(_ searchController: UISearchController) {
+        shownItems = store.items
+    }
 }
