a36402476f74f70b2a6fc4e5bb6017fb89df5e2f
Mikey Ward <mw@mikey.io>
2023-11-13 14:50:16 +0800

    Add error handling example to SomeObject

--- Quiz/SomeObject.swift
+++ Quiz/SomeObject.swift
@@ -3,4 +3,35 @@ import Foundation
 
 class SomeObject {
     weak var otherObject: SomeObject?
+    
+    func doWork() {
+        do {
+            let area1 = try computeArea(length: 5, width: 5)
+            let area2 = try computeArea(length: -5, width: 5)
+        } catch {
+            print("Error occurred: \(error)")
+        }
+
+    }
+    
+    func computeArea(length: Double, width: Double) throws -> Double {
+        guard length > 0 else {
+            throw ComputationError.invalidLength(length)
+        }
+        guard width > 0 else {
+            throw ComputationError.invalidWidth(width)
+        }
+        
+        return length * width
+    }
 }
+
+extension SomeObject {
+    
+    enum ComputationError: Error {
+        case invalidLength(Double)
+        case invalidWidth(Double)
+    }
+    
+}
+
