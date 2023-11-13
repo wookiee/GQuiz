f1fc1547ab0857a75f80974c542c870e9a0c1613
Mikey Ward <mw@mikey.io>
2023-11-13 14:52:29 +0800

    Implement data persistence & Codable in QuizItem

--- Quiz/Model/QuizItem.swift
+++ Quiz/Model/QuizItem.swift
@@ -1,5 +1,18 @@
 
-struct QuizItem {
+struct QuizItem: Codable {
     let question: String
     let answer: String
 }
+
+#if DEBUG
+extension QuizItem {
+    static var sampleItems: [QuizItem] {[
+        QuizItem(question: "What's 7+7?",
+                 answer: "14"),
+        QuizItem(question: "What is cognac made from?",
+                 answer: "Grapes"),
+        QuizItem(question: "What is the speed of light? Are you sure? Are you really sure?",
+                 answer: "186k miles/sec")
+    ]}
+}
+#endif
--- Quiz/Model/QuizItemStore.swift
+++ Quiz/Model/QuizItemStore.swift
@@ -3,23 +3,50 @@ import Foundation
 
 class QuizItemStore {
     
-    var items: [QuizItem]
-    
+    var items: [QuizItem] = []
     static var shared = QuizItemStore()
+    
+    static var fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(component: "items.plist")
+    
     
     private init(items: [QuizItem]) {
         self.items = items
     }
     
-    private init() {
-        self.items = [
-            QuizItem(question: "What's 7+7?",
-                     answer: "14"),
-            QuizItem(question: "What is cognac made from?",
-                     answer: "Grapes"),
-            QuizItem(question: "What is the speed of light? Are you sure? Are you really sure?",
-                     answer: "186k miles/sec")
-        ]
+    private init() {}
+    
+    
+    
+    func save() {
+        do {
+            let encoder = PropertyListEncoder()
+            let data: Data = try encoder.encode(items)
+            try data.write(to: QuizItemStore.fileURL)
+        } catch {
+            print(error)
+        }
+    }
+    
+    @discardableResult
+    private func lookBusy(for duration: TimeInterval) -> Double {
+        var result = 0.0
+        var stillBusy = true
+        DispatchQueue.global().asyncAfter(deadline: .now() + duration) { stillBusy = false }
+        while stillBusy {
+            result = sin(sin(sin(sin(sin(sin(sin(Double.random(in: 0...1))))))))
+        }
+        return result
+    }
+    
+    
+    func load() {
+        do {
+            let decoder = PropertyListDecoder()
+            let data = try Data(contentsOf: QuizItemStore.fileURL)
+            items = try decoder.decode([QuizItem].self, from: data)
+        } catch {
+            print(error)
+        }
     }
 
 }
